5bf334340a2f763c1070002c30d66117b94fda57
[ss-oh] <[tlstmddh12@gmail.com]>
2024-09-01 23:16:15 +0900

    ver0.1

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-const multer = require('multer');
+const { Server } = require('socket.io');
 const path = require('path');
 
-// Express 앱 및 HTTP 서버 초기화
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
-// 정적 파일 제공을 위해 'public' 디렉토리 사용
-app.use(express.static('public'));
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Multer를 사용하여 파일 업로드 설정
-const upload = multer({ dest: 'uploads/' });
-
-// POST 요청을 처리하여 메시지와 이미지 파일을 서버로부터 수신
-app.post('/send', upload.single('image'), (req, res) => {
-    const { message, nickname } = req.body;
-    const image = req.file ? `/uploads/${req.file.filename}` : null;
-
-    // 클라이언트로부터 받은 메시지와 이미지를 모든 클라이언트에 브로드캐스트
-    io.emit('chat message', { message, nickname, image });
-
-    res.json({ status: 'success' }); // 응답으로 성공 상태 전송
-});
-
-// 업로드된 이미지 파일 제공을 위해 'uploads' 디렉토리 사용
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Socket.IO 연결 설정
+// 클라이언트 소켓 연결 처리
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', msg);
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
